2eb4ca029226d67419dbc20ddb5e894457814cf6
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-11-18 08:21:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
 n = int(input("Enter the value of n: "))
 print("The output is as follows: \n")
-for i in (1,n+1):
+for i in range(1,n+1):
     output = " "
     if i % 3 == 0 and i % 5 == 0:
         output += "FizzBuzz"
