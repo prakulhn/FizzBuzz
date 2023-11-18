28afa4e2624bc54b45f62c5696b11376a1a02922
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-11-18 08:30:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
 n = int(input("Enter the value of n: "))
-print("The output is as follows: \n")
+print("The output is as follows: ")
 for i in range(1,n+1):
     output = " "
     if i % 3 == 0 and i % 5 == 0:
