72c93f395412bcf6d85f4872f56dfe4649a774ec
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-11-18 08:27:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ for i in range(1,n+1):
     output = " "
     if i % 3 == 0 and i % 5 == 0:
         output += "FizzBuzz"
-    if i % 3 == 0:
+    elif i % 3 == 0:
         output += "Fizz"
-    if i % 5 == 0:
+    elif i % 5 == 0:
         output += "Buzz"
     else:
         output += str(i)
